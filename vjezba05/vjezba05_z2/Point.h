8d1dbbd2a0cb629991643187404dcdd4d16958b6
Nikola Kelava <nikola.kelava94@gmail.com>
2020-06-03 01:31:11 +0200

    added 5th assignment

--- vjezba05/vjezba05_z2/Point.h
+++ vjezba05/vjezba05_z2/Point.h
@@ -6,6 +6,7 @@ private:
 public:
 	Point(double x = 0, double y = 0) : x(x), y(y) {}
 	Point(const Point& p) { x = p.x; y = p.y; }
+
 	void set_XY(double x, double y) { this->x = x; this->y = y; } // made it just to play with shapes, so I don't need to call setX and setY for every change of whole point
 	void setX(double x) { this->x = x; }
 	double getX() const {return x; }
